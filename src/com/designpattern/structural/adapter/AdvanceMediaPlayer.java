c34ef6377ca133e664fbe6952cfc4be534b88bc3
naimesh-shah <naimesh.shah@hcl.com>
2022-05-13 02:24:36 +0530

    Adapter design pattern

---
+++ src/com/designpattern/structural/adapter/AdvanceMediaPlayer.java
@@ -0,0 +1,7 @@
+package com.designpattern.structural.adapter;
+
+public interface AdvanceMediaPlayer {
+
+	public void playVlc(String fileName);
+	public void playMp4(String fileName);
+}
---
+++ src/com/designpattern/structural/adapter/MediaPlayer.java
@@ -0,0 +1,6 @@
+package com.designpattern.structural.adapter;
+
+public interface MediaPlayer {
+
+	public void play(String audioType, String fileName);
+}
